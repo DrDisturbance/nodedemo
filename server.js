cc4f448bcb8a911a6e1be52243c1d8fbf96dfdf9
DrDisturbance <jharveycoding@gmail.com>
2022-10-28 15:38:17 -0600

    Node stuff
    stuff

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require("http");
 http.createServer(function(request, response) {    
    response.writeHead(200, {"Content-Type": "text/html"});
    response.write("<h1>Hello, Node.js!</h1>");    
-   response.write("<h2>Josh Harvey - x87b678</h2>");    
+   response.write("<h3>Josh Harvey - x87b678</h3>");    
    response.write("<p>Running Node Server on port 3071</p>");    
    response.end();
 }).listen(3071);
